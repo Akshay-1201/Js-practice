4bd48ffe1f0dcef61b35bd804df723e764fa1184
Akshay-1201 <akshaykute12@gmail.com>
2023-02-02 20:59:21 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,14 @@
+let  userName = 'Akshay Kute';
+console.log("Datatype of", userName , "is " , typeof userName );
+
+let number = 12
+console.log("Datatype of", number , "is " , typeof number );
+
+let var2 = true;
+console.log("Data type of", var2 , "is" , typeof var2);
+
+let century;
+console.log("Data type of course is" , typeof course);
+
+let marks = null;
+console.log("Data type of marks is", typeof marks);
