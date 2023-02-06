dc25690249d2e2672a57db0b6c667e850dfedc5a
Akshay-1201 <akshaykute12@gmail.com>
2023-02-06 21:16:18 +0530

    commit

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+//DATATYPES 
+// null,number,symbol,string,boolean,bigint,undefined
+//premitive datatype
+let a = null;
+let b = 12;
+let c = true;
+let d = BigInt("34");
+let e = "Aksahy";
+let f = Symbol("I am a symbol");
+let g = undefined;
+console.log(a,b,c,d,e,f,g);
+console.log(typeof b);
+
+//Objects in js
+
+const item = {
+    "abc": true,
+    "xyz": false,
+    "akshay": 8,
+    "k": undefined
+}
+console.log(item["k"])
+// PRACTICE
